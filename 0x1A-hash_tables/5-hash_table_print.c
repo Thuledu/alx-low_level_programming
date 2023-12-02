31859656019d1e3062a2612e55cf2123c3216d72
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-12-01 16:10:36 -0800

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -4,38 +4,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - Prints a hash table.
- * @ht: A pointer to the hash table to print.
- */
-#include "hash_tables.h"
-
-/**
- * hash_table_print - Prints the key/value pairs in the hash table
- * @ht: The hash table
- */
+* hash_table_print - Prints the key/value pairs in the hash table
+* @ht: The hash table
+*/
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int index;
-    hash_node_t *node;
-    int first = 1;
+	unsigned long int index;
+	hash_node_t *node;
+	int first = 1;
 
-    if (ht == NULL)
-        return; /* If ht is NULL, don't print anything */
+	if (ht == NULL)
+		return; /* If ht is NULL, don't print anything */
 
-    printf("{");
-    for (index = 0; index < ht->size; index++)
-    {
-        node = ht->array[index];
-        while (node != NULL)
-        {
-            if (first)
-                first = 0;
-            else
-                printf(", ");
-            printf("'%s': '%s'", node->key, node->value);
-            node = node->next;
-        }
-    }
-    printf("}\n");
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		node = ht->array[index];
+		while (node != NULL)
+		{
+			if (first)
+				first = 0;
+			else
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+		}
+	}
+	printf("}\n");
 }
-
